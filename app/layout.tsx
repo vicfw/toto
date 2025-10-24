65f273c2eb517b35f19cdb6422a1213bf2371a31
mehrshad0289 <mehrshad02899@gmail.com>
2025-10-24 21:08:59 +0330

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,16 @@
 import Providers from "./providers";
 import "./globals.css";
 
-<<<<<<< HEAD
-=======
+
 import { Roboto } from "next/font/google";
 import SubMenu from "@/components/Layout/SubMenu";
->>>>>>> 036193e3ebbd54d24432837a3097e5a31adcfca6
+
 export const metadata = {
   title: "BetWinner",
   description: "Next.js + React Query Example",
 };
-<<<<<<< HEAD
 
-=======
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  display: "swap",
-});
->>>>>>> 036193e3ebbd54d24432837a3097e5a31adcfca6
+
 export default function RootLayout({
   children,
 }: {
@@ -26,7 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
-<<<<<<< HEAD
       <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Lateef:wght@200;300;400;500;600;700;800&display=swap"
@@ -34,17 +25,10 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <Providers>{children}</Providers>
-=======
-      <body className={roboto.className}>
-        <Providers>
-          <div className=" pb-[80px]">
-            {children}
-          </div>
-          <SubMenu />
-        </Providers>
->>>>>>> 036193e3ebbd54d24432837a3097e5a31adcfca6
+        <div className=" pb-[80px">
+          <Providers>{children}</Providers>
+        </div>
       </body>
-    </html>
+    </html >
   );
 }
