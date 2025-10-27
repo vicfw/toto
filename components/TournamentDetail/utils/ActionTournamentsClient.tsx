54cfa6c50fd6c3b9ba4a97eb469e672886b86882
mehrshad0289 <mehrshad02899@gmail.com>
2025-10-27 16:11:37 +0330

    tournament

--- components/TournamentDetail/utils/ActionTournamentsClient.tsx
+++ components/TournamentDetail/utils/ActionTournamentsClient.tsx
@@ -5,7 +5,7 @@
 import Button from "@/components/App/Button";
 
 interface ActionTournamentsClientProps {
-  matchesWithSelections: number ;
+  matchesWithSelections: number;
   totalMatches: number;
   totalSelections: number;
   totalLines: number;
@@ -30,30 +30,29 @@ export default function ActionTournamentsClient({
   handleRandom,
   handleSubmitBets,
 }: ActionTournamentsClientProps) {
-
-
   return (
     <div className=" fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 md:sticky md:bottom-0 md:left-auto md:right-auto md:border md:border-gray-200 md:rounded-lg md:shadow-sm md:px-4 md:pt-4 md:pb-4 md:z-10 md:mt-6 shadow-custom">
       <div className="px-4 pt-4 pb-2 md:px-0 md:pt-0">
-        <div className="flex items-center justify-between bg-gray-50 rounded-lg p-3 md:p-4">
-          <div className="flex items-center gap-2 md:gap-3">
-            <svg
-              viewBox="0 0 24 24"
-              className="w-4 h-4 md:w-5 md:h-5 text-deep-blue"
-            >
-              <path
-                fill="currentColor"
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
-              />
-            </svg>
-            <div className="flex flex-col">
-              <span className="text-xs md:text-sm text-gray-600">
+        <div className="flex flex-col items-center justify-between bg-gray-50 rounded-lg p-3 md:p-4 w-full">
+          <div className="flex items-center justify-between gap-2 md:gap-3 w-full text-xs md:text-sm">
+            <div className="flex ">
+              <svg
+                viewBox="0 0 24 24"
+                className="w-4 h-4 md:w-5 md:h-5 text-deep-blue"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+                />
+              </svg>
+              <span className=" text-gray-600">
                 تعداد انتخاب شده
               </span>
-              <span className="text-sm md:text-base font-semibold text-gray-900">
-                {matchesWithSelections} از {totalMatches} مسابقه
-              </span>
             </div>
+
+            <span className="font-semibold text-gray-900">
+              {matchesWithSelections} از {totalMatches} مسابقه
+            </span>
           </div>
           {/* <div className="text-left">
             <div className="text-xs md:text-sm text-gray-600">
@@ -65,40 +64,36 @@ export default function ActionTournamentsClient({
           </div> */}
 
           {/* line_coefficient */}
-            <div className="text-left">
-              <div className="text-xs md:text-sm text-gray-600">
-                ضریب خط
-              </div>
-              <div className="text-lg md:text-xl font-bold text-deep-blue">
-                {line_coefficient}
-              </div>
+          <div className="text-left flex items-center justify-between w-full">
+            <div className=" text-gray-600">ضریب خط</div>
+            <div className="text-lg md:text-xl font-bold text-deep-blue">
+              {line_coefficient}
             </div>
-            {/* totalLines */}
-            <div className="text-left">
-              <div className="text-xs md:text-sm text-gray-600">
-                تعداد خطوط
-              </div>
-              <div className="text-lg md:text-xl font-bold text-deep-blue">
-                {totalLines}
-              </div>
+          </div>
+          {/* totalLines */}
+          <div className="text-left flex items-center justify-between w-full">
+            <div className=" text-gray-600">تعداد خطوط</div>
+            <div className="text-lg md:text-xl font-bold text-deep-blue">
+              {totalLines}
             </div>
-            {/* totalPrice */}
-            <div className="text-left">
-              <div className="text-xs md:text-sm text-gray-600">
-                قیمت کل
-              </div>
-              <div className="text-lg md:text-xl font-bold text-deep-blue">
-                {totalPrice.toFixed(2).toLocaleString("fa-IR")} 
-                <span className="text-xs text-gray-600">دلار</span>
-              </div>
+          </div>
+          {/* totalPrice */}
+          <div className="text-left flex items-center justify-between w-full ">
+            <div className=" text-gray-600">قیمت کل</div>
+            <div className="text-lg md:text-xl font-bold text-deep-blue">
+              {totalPrice.toFixed(2).toLocaleString("fa-IR")}
+              <span className="text-xs text-gray-600">دلار</span>
             </div>
+          </div>
         </div>
         <div className="flex items-center justify-between bg-gray-50 rounded-lg p-3  border border-gray-200 mt-2">
           <span className="text-xs text-gray-600">موجودی فعلی شما:</span>
           <span className=" font-bold text-deep-blue text-xs">
-            {balance
-              ? `${parseFloat(balance).toLocaleString("fa-IR")} دلار`
-              : <span className=""> در حال دریافت...</span>}
+            {balance ? (
+              `${parseFloat(balance).toLocaleString("fa-IR")} دلار`
+            ) : (
+              <span className=""> در حال دریافت...</span>
+            )}
           </span>
         </div>
       </div>
